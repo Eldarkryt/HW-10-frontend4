e7bd1065ed2f7988c7e00c964f655091dda1eb83
Eldarkryt <eldarprisaznuk11@gmail.com>
2026-08-26 19:02:43 +0300

    do home-work

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+// let time = 1;
+
+// const intervalId = setInterval(() => {
+//   time++;
+//     console.log('Сповіщення про роботу таймера');
+//     if (time === 5) {
+//         clearInterval(intervalId)
+//     }
+
+// }, 1000);
+
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+
+let timeTask2 = 1;
+
+const intervalId2 = setInterval(() => {
+    timeTask2++
+    if (timeTask2 % 3) {
+        box1.classList.add("box1Big")
+    }
+     if (timeTask2 % 2) {
+       box1.classList.remove("box1Big");
+     }
+        if (timeTask2 % 3) {
+          box2.classList.add('box2Long');
+        }
+        if (timeTask2 % 2) {
+          box2.classList.remove('box2Long');
+        }
+        if (timeTask2 % 3) {
+          box3.classList.add('box3Wide');
+        }
+        if (timeTask2 % 2) {
+          box3.classList.remove('box3Wide');
+        }
+}, 1000)
